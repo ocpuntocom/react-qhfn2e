80cb7789c3765deb678bb820001ad814aa4f9e91
ocpuntocom <81719509+ocpuntocom@users.noreply.github.com>
2023-05-07 19:06:01 -0400

    1 file modified, 2 added

--- src/index.js
+++ src/index.js
@@ -1,25 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import Page from './Page';
 import './style.css';
-
-
-function Header() {
-  const reloj = Date().toLocaleString();
-  return <h1 className="orange"> Hello {reloj} </h1> ;
-}
-function Page() {
-  return ( 
-    <>
-    <Header /> 
-    <p> React es fino</p>
-    <p>Mi sankalpa</p>
-    </>
-    );
-}
-
-
-
-
 const rootEl = document.querySelector('#root');
 const root = ReactDOM.createRoot(rootEl);
 setInterval(function(){
