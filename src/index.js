0c054366421a20fbe036d109770230b3af8d2ab8
ocpuntocom <81719509+ocpuntocom@users.noreply.github.com>
2023-05-07 19:07:22 -0400

    1 file modified

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Page from './Page';
 import './style.css';
+
 const rootEl = document.querySelector('#root');
 const root = ReactDOM.createRoot(rootEl);
 setInterval(function(){
